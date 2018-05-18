3ca096b92e7229b751dda80fcd6c58e01ba830aa
Neryungrisky <disperagioia@gmail.com>
2018-05-18 12:44:36 -0400

    Update Chess.java

--- src/Chess.java
+++ src/Chess.java
@@ -6,4 +6,10 @@ public class Chess {
 		board = new Piece[8][8];
 		//Initialize pieces on board
 	}
+	
+	public ArrayList<Piece> moves(int x, int y){
+		switch(board[x][y].getType()){
+			
+		}
+	}
 }
