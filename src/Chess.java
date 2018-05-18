26569b00f8479c97f1fb0090e16878e84dcfa01a
Neryungrisky <disperagioia@gmail.com>
2018-05-18 12:50:25 -0400

    Update Chess.java

--- src/Chess.java
+++ src/Chess.java
@@ -10,7 +10,37 @@ public class Chess {
 	
 	public ArrayList<Piece> moves(int x, int y){
 		switch(board[x][y].getType()){
-			
+		case 'k':
+			return movesK(x, y);
+		case 'q':
+			return movesQ(x, y);
+		case 'b':
+			return movesB(x, y);
+		case 'n':
+			return movesN(x, y);
+		case 'r':
+			return movesR(x, y);
+		default://case 'p':
+			return movesP(x, y);
 		}
+	}
+	
+	public ArrayList<Piece> movesK(int x, int y){
+		return null;
+	}
+	public ArrayList<Piece> movesQ(int x, int y){
+		return null;
+	}
+	public ArrayList<Piece> movesB(int x, int y){
+		return null;
+	}
+	public ArrayList<Piece> movesN(int x, int y){
+		return null;
+	}
+	public ArrayList<Piece> movesR(int x, int y){
+		return null;
+	}
+	public ArrayList<Piece> movesP(int x, int y){
+		return null;
 	}
 }
