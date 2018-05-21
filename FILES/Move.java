ad5f6bad2499ed4751cc2c9f33192b319a05131a
Neryungrisky <disperagioia@gmail.com>
2018-05-21 13:31:28 -0400

    Create Move.java

---
+++ FILES/Move.java
@@ -0,0 +1,18 @@
+
+public class Move {
+	int xTo;
+	int yTo;
+	
+	public Move(int x, int y){
+		xTo = x;
+		yTo = y;
+	}
+	
+	public int getX(){
+		return xTo;
+	}
+	
+	public int getY(){
+		return yTo;
+	}
+}
