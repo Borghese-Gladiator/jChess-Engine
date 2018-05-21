ae0ad66d4cc26939586f74a87958222f06bef235
Neryungrisky <disperagioia@gmail.com>
2018-05-21 13:36:00 -0400

    Update Piece.java

--- FILES/Piece.java
+++ FILES/Piece.java
@@ -2,11 +2,9 @@
 public abstract class Piece {
 	char type;
 	boolean isWhite;
-	boolean hasMoved;
 	
 	public Piece(boolean isWhite){
 		this.isWhite = isWhite;
-		hasMoved = false;
 	}
 	
 	public abstract char getType();
