cb9ed231d601aff77c522a7abf69863925521472
Neryungrisky <disperagioia@gmail.com>
2018-05-21 13:43:30 -0400

    Create Rook.java

---
+++ FILES/Rook.java
@@ -0,0 +1,22 @@
+
+public class Rook extends Piece {
+	final static char type = 'r';
+	boolean hasMoved;
+	
+	public Rook(boolean isWhite, char type){
+		super(isWhite);
+		hasMoved = false;
+	}
+	
+	public char getType(){
+		return type;
+	}
+	
+	public boolean getHasMoved(){
+		return hasMoved;
+	}
+	
+	public void setHasMoved(boolean hasMoved){
+		this.hasMoved = hasMoved;
+	}
+}
