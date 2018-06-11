0c32c64359b018fd41d1bf1262bcb6dde0ab25ef
sophia78 <39414143+sophia78@users.noreply.github.com>
2018-06-11 13:55:07 -0400

    some stuff

--- src/main/java/BoardMovement/aiValidMoves.java
+++ src/main/java/BoardMovement/aiValidMoves.java
@@ -3,17 +3,9 @@ package BoardMovement;
 import java.util.ArrayList;
 
 public class aiValidMoves {
-	private static boolean[][]BlackAttack = new boolean[8][8];
-	private static boolean [][]WhiteAttack = new boolean[8][8];
-	private static Position BK;
-	private static Position WK;
-	
 	private static void checkmove(Piece[][]board, Position current, ArrayList<Position> list){
 		boolean white = board[current.getX()][current.getY()].isWhite();
-		if(white == true)
-			BlackAttack[current.getX()][current.getY()] =true;
-		else
-			WhiteAttack[current.getX()][current.getY()] =true;
+		
 		for(int i = 0; i<list.size(); i++){
 			if(board[list.get(i).getX()][list.get(i).getY()].isWhite()== white)
 				board[list.get(i).getX()][list.get(i).getY()].setdefend(board[current.getX()][current.getY()].actionvalue());
