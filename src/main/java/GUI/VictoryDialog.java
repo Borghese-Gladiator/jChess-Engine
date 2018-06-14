282931143a72ca09c6930bc5ec4b5ecf14e077af
timothy-shee <timothy_shee@hotmail.com>
2018-06-13 23:55:11 -0400

    GUI Upload Dialog Outlines

---
+++ src/main/java/GUI/VictoryDialog.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class VictoryDialog extends JDialog
+{
+	public VictoryDialog(JFrame owner) {
+        super(owner, "VICTORY", true);
+	}
+}
